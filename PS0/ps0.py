d65c94e2be0a66933648b169ad8bd47a755f09e7
ovega141 <vega00087@gmail.com>
2023-01-19 20:50:53 -0500

    finished pset 0

---
+++ PS0/ps0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 18:25:56 2023
+
+@author: Octavio Vega
+"""
+
+import numpy as np
+
+print('Enter number x: ')
+x = float(input())
+
+print('Enter number y: ')
+y = float(input())
+
+print('x**y = ', x**y)
+print('log(x) = ', np.log(x)/np.log(2)) # convert to base 2 log
