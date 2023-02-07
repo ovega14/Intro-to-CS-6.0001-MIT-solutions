3f366de1262e5bea043f7357637f0e578b9b2518
ovega141 <vega00087@gmail.com>
2023-02-07 17:16:36 -0500

    added test cases to ps4a

--- PS4/ps4a.py
+++ PS4/ps4a.py
@@ -28,9 +28,9 @@ def get_permutations(sequence):
     if n == 1:
         return [sequence]
     
-    # recursive step: get all permutations of length n-1 sequence and shift first character around
+    # recursive step: get all unique permutations of length n-1 sequence and shift first character around
     else:
-        return [s[:i] + sequence[0] + s[i:] for i in range(n) for s in get_permutations(sequence[1:])]
+        return [*set([s[:i] + sequence[0] + s[i:] for i in range(n) for s in get_permutations(sequence[1:])])]
         
 if __name__ == '__main__':
 #    #EXAMPLE
@@ -43,7 +43,31 @@ if __name__ == '__main__':
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
+    # case 1: 'abc'
+    example_input_1 = 'abc'
+    expected_output_1 = ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+    actual_output_1 = get_permutations(example_input_1)
+    if sorted(expected_output_1) == sorted(actual_output_1):
+        print("Test case 1 PASSED")
+    else:
+        print(f"Test case 1 FAILED: expected {expected_output_1} but received {actual_output_1}")
     
+    # case 2: 'odd'
+    example_input_2 = 'odd'
+    expected_output_2 = ['odd', 'dod', 'ddo']
+    actual_output_2 = get_permutations(example_input_2)
+    if sorted(expected_output_2) == sorted(actual_output_2):
+        print("Test case 2 PASSED")
+    else:
+        print(f"Test case 2 FAILED: expected {expected_output_2} but received {actual_output_2}")
+        
+    # case 3: 'xy'
+    example_input_3 = 'xy'
+    expected_output_3 = ['xy', 'yx']
+    actual_output_3 = get_permutations(example_input_3)
+    if sorted(expected_output_3) == sorted(actual_output_3):
+        print("Test Case 3 PASSED")
+    else:
+        print(f"Test Case 3 FAILED: expected {expected_output_3} but received {actual_output_3}")
     
 
